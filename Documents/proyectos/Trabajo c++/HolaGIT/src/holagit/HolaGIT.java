b05afea2089f9f35a34e163dfc636d6026f0cb44
Julian Garcia <julian.garcia@cesjuanpablosegundo.es>
2019-01-25 12:17:42 +0100

    Version 1

---
+++ Documents/proyectos/Trabajo c++/HolaGIT/src/holagit/HolaGIT.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holagit;
+
+/**
+ *
+ * @author Usuario DAM 1
+ */
+public class HolaGIT {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
